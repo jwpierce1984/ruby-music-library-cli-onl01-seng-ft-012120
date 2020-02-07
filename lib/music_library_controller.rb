0de426d2ccafc6c01e5b368a866ed867e3ffddc8
Justin Pierce <jwpierce1984@gmail.com>
2020-02-07 17:29:44 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -6,6 +6,7 @@ class MusicLibraryController
   end
   
   def call
+    input = ""
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -18,6 +19,21 @@ class MusicLibraryController
   
     input = gets.strip
     
+    case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
+    end
   end
   
   
